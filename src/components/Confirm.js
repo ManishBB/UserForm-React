644953643c0de9da6d7091fa46b32295e8eebb21
Manish <mbb7224@gmail.com>
2021-10-04 19:01:53 +0530

    Added basic logic

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -1,11 +1,72 @@
-import React from 'react'
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import { List, ListItem, ListItemText } from '@material-ui/core/';
+import Button from '@material-ui/core/Button';
 
 const Confirm = (props) => {
+
+    const continues = e => {
+        e.preventDefault();
+        props.nextStep();
+      };
+
+    const back = e => {
+        e.preventDefault();
+        props.prevStep();
+      };
+        
+      const {
+        values: { firstName, lastName, email, occupation, city, bio }
+      } = props;
+
     return (
-        <div>
-            
-        </div>
-    )
+      <MuiThemeProvider>
+        <>
+          <Dialog
+            open
+            fullWidth
+            maxWidth='sm'
+          >
+            <AppBar title="Confirm User Data" />
+            <List>
+              <ListItem>
+                <ListItemText primary="First Name" secondary={firstName} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Last Name" secondary={lastName} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Email" secondary={email} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Occupation" secondary={occupation} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="City" secondary={city} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Bio" secondary={bio} />
+              </ListItem>
+            </List>
+            <br />
+
+            <Button
+              color="secondary"
+              variant="contained"
+              onClick={back}
+            >Back</Button>
+
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={continues}
+            >Confirm & Continue</Button>
+          </Dialog>
+        </>
+      </MuiThemeProvider>
+    );
 }
 
 export default Confirm
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,11 +1,64 @@
-import React from 'react'
+import React , { useState } from "react";
+import FormUserDetails from "./FormUserDetails";
+import FormPersonalDetails from "./FormPersonalDetails";
+import Confirm from "./Confirm";
+import Success from "./Success";
 
 function UserForm() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [step, setStep] = useState(1);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [occupation, setOccupation] = useState("");
+  const [city, setCity] = useState("");
+  const [bio, setBio] = useState("");
+
+  const [details, setDetails] = useState({firstName : "", lastName : "", email : "", occupation : "", city : "", bio : ""})
+
+  const values = { firstName, lastName, email, occupation, city, bio };
+  const nextStep = () => {
+    setStep(step + 1);
+  };
+
+  const prevStep = () => {
+    setStep(step - 1);
+  };
+
+  const handleChange = (input) => (e) => {
+    setDetails({...details , [e.target.name] : e.target.value})
+  };
+
+  switch (step) {
+    case 1:
+      return (
+        <FormUserDetails
+          nextStep={nextStep}
+          handleChange={handleChange}
+          values={values}
+        />
+      );
+    case 2:
+      return (
+        <FormPersonalDetails
+          nextStep={nextStep}
+          prevStep={prevStep}
+          handleChange={handleChange}
+          values={values}
+        />
+      );
+    case 3:
+      return (
+        <Confirm
+          nextStep={nextStep}
+          prevStep={prevStep}
+          values={values}
+        />
+      );
+    case 4:
+      return <Success />;
+    default:
+      console.log("This is a multi-step form built with React.");
+  }
 }
 
-export default UserForm
+export default UserForm;
